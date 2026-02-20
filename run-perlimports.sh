7378f0c5ed6cb7e084ab4b87885991be7126a68b
Reini Urban <reini@spexai.com>
2026-02-20 08:40:27 +0100

    make it non-intrusive
    mv back the backup if nothing changed to avoid rebuilds
    and changed timestamps. no .bak backup since we are on git anyway,
    analog to the clang-format hook.
    
    with perlimports use cp -p to preserve the timestamp at least.

--- run-perlimports.sh
+++ run-perlimports.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Run perlimports on perl scripts.
+# Either with option --inplace-edit or --lint on some files.
 
 set -eu
 
@@ -10,7 +11,29 @@ if ! command -v "${cmd}" >/dev/null 2>&1; then
     exit 1
 fi
 
-if ! output=$("${cmd}" "$@" 2>&1); then
-    echo "${output}"
-    exit 1
-fi
+opts=
+need_cp=
+case $1 in
+    --inplace-edit) opts="$1"; need_cp=1; shift ;;
+    -*) opts="$1"; shift ;;
+esac
+
+for file in "$@"; do
+    if [ -n "${need_cp}" ]; then
+        cp -fp "${file}" "${file}.bak"
+    fi
+    if ! output=$("${cmd}" ${opts} "${file}" 2>&1); then
+        echo "${output}"
+        exit 1
+    fi
+    if [ -n "${need_cp}" ]; then
+        if cmp "$file" "${file}.bak" >/dev/null 2>&1; then
+            # nothing changed
+            mv "${file}.bak" "${file}"
+        else
+            # we have it in git
+            rm -f "${file}.bak"
+        fi
+    fi
+done
+
